5fc3ac99d540ac38357d89de8a44012b8ddb1769
Marcus Lundberg <tairoman@users.noreply.github.com>
2016-10-06 12:22:33 +0200

    Update M8_vm.c

--- src/M8_vm.c
+++ src/M8_vm.c
@@ -14,15 +14,20 @@
 
 bool running = true;
 
+/*
+    Reads a file and returns the content as a string.
+    TODO: Make more generic with malloc size.
+          (Take another argument)
+*/
 char *M8_read_file(char *filename) {
     int ch;
     FILE *fp;
-    char *s = malloc(2*256*2*sizeof(char));
+    char *s = malloc(2*256*2*sizeof(char)); // Max 256 instructions * 2 hex chars * approx spaces
     int index = 0;
 
     fp = fopen(filename,"r");
 
-    assert(fp!=NULL);
+    assert(fp!=NULL); // TODO: Change to nice error message
 
     while( ( ch = fgetc(fp) ) != EOF ){
         s[index++] = (char) ch;
@@ -33,7 +38,10 @@ char *M8_read_file(char *filename) {
     return s;
 
 }
-
+/*
+    Returns the 7th bit of a number. Used to check for overflow.
+    Example: 7th bit on op1 and op2 is set but not on result -> overflow
+*/
 inline bool M8_get_bit_seven(int16_t x) {
     return ((x>>7) & 1) == 1;
 }
